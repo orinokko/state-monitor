611cecb66113799a73ff4ca4e3be140ad0c19c38
Orinoko <nexus.86@mail.ru>
2019-05-02 13:30:35 +0300

    init

--- src/config/state-monitor.php
+++ src/config/state-monitor.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'use-local' => env('STATE_MONITOR_LOCAL_EMAIL', 'a@whiteweb.ua'),
-    'alert-email' => env('STATE_MONITOR_ALERT_EMAIL', 'a@whiteweb.ua'),
+    'use-local' => env('STATE_MONITOR_LOCAL_EMAIL', '1'),
+    'alert-email' => env('STATE_MONITOR_ALERT_EMAIL', ''),
 ];
