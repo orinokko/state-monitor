c7ee3506a94651d92c0286ff0cb954ca54d2513f
Orinoko <nexus.86@mail.ru>
2019-05-02 17:39:13 +0300

    init

--- tests/Unit/TestTest.php
+++ tests/Unit/TestTest.php
@@ -1,18 +1,15 @@
 <?php
-namespace Orinoko\StateMonitor\Test;
+namespace Orinoko\StateMonitor\Test\Unit;
 
 use Monitor;
 use Orinoko\StateMonitor\Tests\TestCase;
-
+/*
 class TestTest extends TestCase
 {
-    /**
-     * Check that the multiply method returns correct result
-     * @return void
-     */
     public function testMultiplyReturnsCorrectValue()
     {
         $this->assertSame(Monitor::checkFacade(), 'facade run');
         $this->assertSame(Monitor::checkFacade(), 'nope');
     }
 }
+*/
