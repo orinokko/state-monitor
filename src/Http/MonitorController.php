ae596925f6454f9a05cf41e7d264220b5b42c879
Orinoko <nexus.86@mail.ru>
2019-05-02 17:56:28 +0300

    init

---
+++ src/Http/MonitorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WW2UA\Monitor\Http;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MonitorController //extends Controller
+{
+    public function index(){
+        dd('installed');
+    }
+}
