69d64926f21a8e4fc66094e6c278a6a8faa6dc2f
Orinoko <nexus.86@mail.ru>
2019-05-02 17:39:48 +0300

    init

--- tests/Unit/TestTest.php
+++ tests/Unit/TestTest.php
@@ -6,7 +6,7 @@ use Orinoko\StateMonitor\Tests\TestCase;
 /*
 class TestTest extends TestCase
 {
-    public function testMultiplyReturnsCorrectValue()
+    public function testFacadeExist()
     {
         $this->assertSame(Monitor::checkFacade(), 'facade run');
         $this->assertSame(Monitor::checkFacade(), 'nope');
