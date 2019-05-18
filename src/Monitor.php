0847f56875b16a16696b75cb6233ead1f3360f0d
Orinoko <nexus.86@mail.ru>
2019-05-18 09:21:35 +0300

    docs

--- src/Monitor.php
+++ src/Monitor.php
@@ -311,17 +311,17 @@ class Monitor
 
     /**
      * Store request info (for errors)
-     * @param  string $time
-     * @param  string $app_id
-     * @param  string $url
-     * @param  string $method
-     * @param  array $params
-     * @param  string $code
-     * @param  string $message
-     * @param  string $file
-     * @param  string $line
-     * @param  array $headers
-     * @param  array $trace
+     * @param  string $time - Current time DATETIME
+     * @param  string $app_id - id of current installation
+     * @param  string $url - url where was this error
+     * @param  string $method - request method
+     * @param  array $params - request params, array(key=>value,key2=>value2)
+     * @param  string $code - response code
+     * @param  string $message - error message
+     * @param  string $file - filepath where was an error
+     * @param  string $line - line of file where was an error
+     * @param  array $headers - request headers, array(key=>value,key2=>value2)
+     * @param  array $trace - error trace, array([file=>path,line=>value])
      * @return array
      */
     public function saveError($time,$app_id,$url,$method,$params,$code,$message,$file,$line,$headers,$trace)
@@ -354,11 +354,11 @@ class Monitor
     }
 
     /**
-     * Prepare request info (for errors)
-     * @param  string $type request direction (incoming|outgoing)
-     * @param  string $url
-     * @param  string $method
-     * @param  array $params
+     * Validate request
+     * @param  string $type - request direction (incoming|outgoing)
+     * @param  string $url - url of the request
+     * @param  string $method - method od the request
+     * @param  array $params - params of the request, array(key=>value,key2=>value2)
      * @return bool
      */
     public function validateRequest($type,$url,$method,$params=[])
@@ -386,11 +386,11 @@ class Monitor
     }
 
     /**
-     * Prepare request info (for errors)
-     * @param  string $message event description
-     * @param  string $url
-     * @param  string $method
-     * @param  array $params
+     * Store custom event
+     * @param  string $message - event description
+     * @param  string $url - url of the request
+     * @param  string $method - method od the request
+     * @param  array $params - params of the request, array(key=>value,key2=>value2)
      * @return array
      */
     public function storeEvent($message,$url='',$method='',$params=[])
@@ -413,10 +413,10 @@ class Monitor
     }
 
     /**
-     * Prepare request info (for errors)
-     * @param  string $query
-     * @param  array $bindings
-     * @param  string $query_time
+     * Store query to log
+     * @param  string $query - sql query
+     * @param  array $bindings - params of the query
+     * @param  string $query_time - time for execution
      * @return array
      */
     public function storeQuery($query,$bindings,$query_time)
